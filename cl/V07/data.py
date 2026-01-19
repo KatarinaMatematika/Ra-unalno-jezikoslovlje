c4959ba3886e1527d0d44b7dab52eceec55e4802
KatarinaMatematika <katarina.pazur98@gmail.com>
2026-01-19 11:33:13 +0100

    Drugi kolokvij

---
+++ cl/V07/data.py
@@ -0,0 +1,32 @@
+"""
+vlastiti (jednostavni) korpusi
+"""
+
+# vlastiti korpus dokumenata
+# primjer 1
+texts1 = [
+    "Penny bought bright blue fishes.",
+    "Penny bought bright blue and orange fish.",
+    "The cat ate a fish at the store.",
+    "Penny went to the store. Penny ate a bug. Penny saw a fish.",
+    "It meowed once at the bug, it is still meowing at the bug and the fish",
+    "The cat is at the fish store. The cat is orange. The cat is meowing at the fish.",
+    "Penny is a fish"      
+]
+
+# primjer 2
+texts2 = ['Penny bought bright blue fishes.',
+ 'Penny bought bright blue and orange bowl.',
+ 'The cat ate a fish at the store.',
+ 'Penny went to the store. Penny ate a bug. Penny saw a fish.',
+ 'It meowed once at the bug, it is still meowing at the bug and the fish',
+ 'The cat is at the fish store. The cat is orange. The cat is meowing at the fish.',
+ 'Penny is a fish.',
+ 'Penny Penny she loves fishes Penny Penny is no cat.',
+ 'The store is closed now.',
+ 'How old is that tree?',
+ 'I do not eat fish I do not eat cats I only eat bugs']
+
+
+
+        
